40a8b80814fa5675d07f6c3a2c2ce12210bc6509
Kml159 <99826221+Kml159@users.noreply.github.com>
2023-12-30 17:28:11 +0300

    DO SQL CONNECTION

---
+++ src/application/Database.java
@@ -0,0 +1,9 @@
+
+package application;
+
+
+
+public class Database {
+
+    
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,22 @@
 package application;
 	
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+
+// SQL imports
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 // Main java
 public class Main extends Application {
@@ -20,7 +33,46 @@ public class Main extends Application {
 		}
 	}
 	
+	// connection URL
+    private static final String URL = "jdbc:mysql://localhost:3306/test?serverTimezone=UTC";
+
+    // database username and password
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1A2b3c4d5e.";
+
+    public static void connectDatabase() {
+        try {
+            // load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // create a connection object
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            // create a statement object
+            Statement statement = connection.createStatement();
+
+            // execute a query and get the result set
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
+
+            // print the result set
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("ID") + " " + resultSet.getString("FIRST_NAME") + " " + resultSet.getString("LAST_NAME"));
+            }
+
+            // close the result set, statement and connection
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            // handle the exception
+            e.printStackTrace();
+        }
+    }
+	
 	public static void main(String[] args) {
+		connectDatabase();
+		
 		launch(args);
 	}
 }
+
