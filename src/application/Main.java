fb92a15f4e4141b809fcde05974d379a338156e9
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2023-12-30 16:11:22 +0300

    .
    sasa

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+// Main java
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
