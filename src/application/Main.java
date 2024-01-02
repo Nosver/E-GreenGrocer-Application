699ea97712c9a72edf86f3761172de9e0df3a5a9
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-01-02 15:46:44 +0300

    gitignore

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,6 @@
 package application;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
