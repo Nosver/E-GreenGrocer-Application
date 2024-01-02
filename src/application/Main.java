5c700a300100a9e1df410c3dccc26545bc7abd59
MasisOznigolyan <oznigolyan3@stu.khas.edu.tr>
2024-01-02 15:44:13 +0300

    to do list added

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,18 @@ import javafx.stage.Stage;
 // Main java
 public class Main extends Application {
 
-	// Comment from me
+	/*
+	 * 
+	-- userinfo ve update ekranı ve backend // Masis
+
+	-- forgot & change password  //doğukan
+
+	-- sign up  //yiğit
+
+	-- product ekranı //kemal
+
+	 
+	 * */
 
 	@Override
 	public void start(Stage primaryStage) {
