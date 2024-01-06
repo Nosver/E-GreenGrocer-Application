39d75dd5f9eaaeef9ff391bc83c44185e613fcc9
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2024-01-06 17:31:45 +0300

    lll

--- src/application/controller/productScreenController.java
+++ src/application/controller/productScreenController.java
@@ -7,8 +7,11 @@ import java.util.ResourceBundle;
 
 import application.DatabaseAdapter;
 import application.model.Product;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,70 +21,58 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class productScreenController {
 
 	@FXML
-    private ResourceBundle resources;
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private FlowPane PRODUCTS_FLOW;
+	@FXML
+	private FlowPane PRODUCTS_FLOW;
 
-    @FXML
-    private TextField PRODUCT_AMOUNT;
+	@FXML
+	private TextField PRODUCT_AMOUNT;
 
-    @FXML
-    private VBox PRODUCT_BOX;
+	@FXML
+	private VBox PRODUCT_BOX;
 
-    @FXML
-    private Group PRODUCT_GROUP;
+	@FXML
+	private Group PRODUCT_GROUP;
 
-    @FXML
-    private ImageView PRODUCT_IMAGE;
+	@FXML
+	private ImageView PRODUCT_IMAGE;
 
-    @FXML
-    private Text PRODUCT_NAME;
+	@FXML
+	private Text PRODUCT_NAME;
 
-    @FXML
-    private Button PRODUCT_PURCHASE;
+	@FXML
+	private Button PRODUCT_PURCHASE;
 
+	public void initialize() {
 
-    @FXML
-    void initialize() {
-        assert PRODUCT_AMOUNT != null : "fx:id=\"PRODUCT_AMOUNT\" was not injected: check your FXML file 'newProductScreen.fxml'.";
-        assert PRODUCTS_FLOW != null : "fx:id=\"PRODUCTS_FLOW\" was not injected: check your FXML file 'newProductScreen.fxml'.";
-        assert PRODUCT_BOX != null : "fx:id=\"PRODUCT_BOX\" was not injected: check your FXML file 'newProductScreen.fxml'.";
-        assert PRODUCT_GROUP != null : "fx:id=\"PRODUCT_GROUP\" was not injected: check your FXML file 'newProductScreen.fxml'.";
-        assert PRODUCT_IMAGE != null : "fx:id=\"PRODUCT_IMAGE\" was not injected: check your FXML file 'newProductScreen.fxml'.";
-        assert PRODUCT_NAME != null : "fx:id=\"PRODUCT_NAME\" was not injected: check your FXML file 'newProductScreen.fxml'.";
-        assert PRODUCT_PURCHASE != null : "fx:id=\"PRODUCT_PURCHASE\" was not injected: check your FXML file 'newProductScreen.fxml'.";
+		DatabaseAdapter db = new DatabaseAdapter();
+		ArrayList<Product> products = null;
+		System.out.println("dcsd");
 
-        DatabaseAdapter db = new DatabaseAdapter();
-        ArrayList<Product> products = null;
-        
-        try {
-        	products = db.getAllProducts();
-			
+		try {
+			products = db.getAllProducts();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-        
-        for(int i = 0; i < products.size(); i++) {
-        	
-        	Product product = products.get(i);
-        	
-        	Group group = PRODUCT_GROUP;
-        	
-        	PRODUCTS_FLOW.getChildren().add(group);
-        	
-        
-        	
-        }
-        
-    }
+
+		for (Product product : products) {
+	        // Create a new Group for each product
+	        Group group = new Group(PRODUCT_GROUP);
+
+	        // Add the Group to the FlowPane
+	        PRODUCTS_FLOW.getChildren().add(group);
+	    }
+
+	}
 
 }
-
