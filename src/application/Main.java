fdae7a49fd8bb1a11c61814e20614e6ca39eabbb
MasisOznigolyan <oznigolyan3@stu.khas.edu.tr>
2024-01-10 21:15:37 +0300

    .

--- src/application/Main.java
+++ src/application/Main.java
@@ -51,7 +51,7 @@ public class Main extends Application {
 	    	Parent root=FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
 			Scene scene= new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/css/Login.css").toExternalForm());
-
+//.
 	        // Set the main FXML file as the scene
 	       
 			
