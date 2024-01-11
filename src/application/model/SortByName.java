0a04de47e60b8d9e11a5bb6ce21ef9acf7cc0ec1
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2024-01-11 22:56:11 +0300

    It probably isn't finished
    .

--- src/application/model/SortByName.java
+++ src/application/model/SortByName.java
@@ -6,7 +6,7 @@ public class SortByName implements Comparator<Product>{
 
 	@Override
 	public int compare(Product o1, Product o2) {
-		return o2.getName().compareTo(o1.getName());
+		return o1.getName().compareTo(o2.getName());
 	}
 
 }
--- table.sql
+++ table.sql
@@ -52,8 +52,37 @@ INSERT INTO `users` (`name`, `password`, `email`, `role`, `address`) VALUES
 
 -- Inserting sample products
 INSERT INTO `Product` ( `name`, `stock`, `price`, `threshold`,`imagePath`) VALUES
-( 'Product A', 50.0, 10.99, 5,'images/product_a.jpg'),
-( 'Product B', 30.0, 5.99, 3,'images/product_b.jpg');
+( 'Apple', 100, 34.90, 20, 'images/apple.png'),
+( 'Pineapple', 40, 79.90, 30, 'images/Pineapple.png'),
+( 'Patato', 400, 19.90, 300, 'images/Patates.jpg'),
+( 'Peach', 70, 39.90 , 50, 'images/Peach.png'),
+( 'Spinach', 50, 44.90, 35, 'images/Ispanak.png'),
+( 'Pepper', 200, 49.90, 150, 'images/Biber.png'),
+( 'Coconut', 40, 39.90, 30, 'images/Coconut.png'),
+( 'Carrot', 130, 37.90, 20, 'images/Havuc.png'),
+( 'Grape', 20, 74.90, 10, 'images/Grape.png'),
+( 'Waterlemon', 10, 8.90, 5, 'images/Watermelon.png'),
+( 'Broccoli', 10, 49.90, 5, 'images/brokoli.png'),
+( 'Zucchini', 60, 49.90, 10, 'images/Kabak.png'),
+( 'Lettuce', 150, 29.90, 100, 'images/Marul.png'),
+( 'Cabbage', 120, 7.99, 90, 'images/Lahana.jpg'),
+( 'Cucumber', 150, 38.90, 100, 'images/Salatalık.png'),
+( 'Onion', 300, 9.90, 200, 'images/Sogan.png'),
+( 'Strawberry', 15, 89.90, 10, 'images/Strawberry.png'),
+( 'Tomato', 200, 45.90, 100, 'images/Tomato.png'),
+( 'Leek', 30, 29.90, 25, 'images/Pırasa.png'),
+( 'Passion Fruit', 30, 29.90, 25, 'images/carkifelek.png'),
+( 'Dragon Fruit', 30, 29.90, 25, 'images/ejder.png'),
+( 'Plum', 30, 29.90, 25, 'images/erik.png'),
+( 'Cherry', 30, 29.90, 25, 'images/kiraz.png'),
+( 'Cauliflower', 30, 29.90, 25, 'images/karnibahar.jpg.png'),
+( 'Mandarine', 30, 29.90, 25, 'images/mandalina.png');
+
+
+
+
+
+
 
 -- Inserting a sample chart
 INSERT INTO `Chart` (`userId`, `totalPrice`, `state`,`date`) VALUES
